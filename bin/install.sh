f03c9a1c9519ca6f13b6445b244334b8ee8ab7ab
damouyal <dadamouyal42@gmail.com>
2020-04-22 17:52:22 +0200

    install.sh created and git repository is created when needed

--- bin/install.sh
+++ bin/install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-to_install= install_makefile drop_makefile 
+to_install="install_makefile drop_makefile gitignore.template .Makefile"
 if [ ! -d ~/bin ]
 then
 	mkdir ~/bin
 fi
 
-for sub in "$to_install"
+for sub in ${to_install}
 do
+	echo "Installing ${sub}"	
 	if [ -d "$sub" ]
 	then
 		cp -r "$sub" ~/bin
